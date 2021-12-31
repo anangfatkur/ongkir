ea41e223c235972e93d9991f415f3e7663dd2859
anangfatkur <anangfatkurrr@gmail.com>
2021-12-31 15:12:46 +0700

    add provinsi

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:convert';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
+import '../province_model.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,14 +15,70 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('Ongkir Ku'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch<Province>(
+            label: "Provinsi",
+            onFind: (filter) async {
+              Uri url =
+                  Uri.parse("https://api.rajaongkir.com/starter/province");
+
+              try {
+                final response = await http.get(
+                  url,
+                  headers: {
+                    "key": "a76d61456e6590daf8173208032be001",
+                  },
+                );
+
+                var data = json.decode(response.body) as Map<String, dynamic>;
+
+                var statusCode = data["rajaongkir"]["status"]["code"];
+
+                if (statusCode != 200) {
+                  throw data["rajaongkir"]["status"]["description"];
+                }
+
+                var listAllProvince =
+                    data["rajaongkir"]["results"] as List<dynamic>;
+
+                var models = Province.fromJsonList(listAllProvince);
+                return models;
+              } catch (err) {
+                print(err);
+                return List<Province>.empty();
+              }
+            },
+            onChanged: (value) => print(value!.province),
+            showSearchBox: true,
+            dropdownSearchDecoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              hintText: "Cari Provinsi...",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+            popupItemBuilder: (context, item, isSelected) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text(
+                  "${item.province}",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              );
+            },
+            itemAsString: (item) => item!.province!,
+          ),
+        ],
       ),
     );
   }
