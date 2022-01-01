c7a4e2a66672bf2117e5fb918b3c07338110fd46
anangfatkur <anangfatkurrr@gmail.com>
2022-01-01 22:12:02 +0700

    add device orientation dan title

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -17,93 +17,95 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
-      body: ListView(
-        padding: EdgeInsets.all(20),
-        children: [
-          buildProvinsi(
-            tipe: "asal",
-          ),
-          Obx(
-            () => controller.hidKotaAsal.isTrue
-                ? SizedBox()
-                : buildKota(
-                    provinsiId: controller.provinsiAsalId.value,
-                    tipe: "asal",
-                  ),
-          ),
-          buildProvinsi(
-            tipe: "tujuan",
-          ),
-          Obx(
-            () => controller.hidKotaTujuan.isTrue
-                ? SizedBox()
-                : buildKota(
-                    provinsiId: controller.provinsiTujuanId.value,
-                    tipe: "tujuan",
-                  ),
-          ),
-          Weight(),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: DropdownSearch<Map<String, dynamic>>(
-              mode: Mode.MENU,
-              showClearButton: true,
-              // showSelectedItems: true,
-              items: [
-                {
-                  "code": "jne",
-                  "name": "Jalur Nugraha Ekakurir(JNE)",
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            buildProvinsi(
+              tipe: "asal",
+            ),
+            Obx(
+              () => controller.hidKotaAsal.isTrue
+                  ? SizedBox()
+                  : buildKota(
+                      provinsiId: controller.provinsiAsalId.value,
+                      tipe: "asal",
+                    ),
+            ),
+            buildProvinsi(
+              tipe: "tujuan",
+            ),
+            Obx(
+              () => controller.hidKotaTujuan.isTrue
+                  ? SizedBox()
+                  : buildKota(
+                      provinsiId: controller.provinsiTujuanId.value,
+                      tipe: "tujuan",
+                    ),
+            ),
+            Weight(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: DropdownSearch<Map<String, dynamic>>(
+                mode: Mode.MENU,
+                showClearButton: true,
+                // showSelectedItems: true,
+                items: [
+                  {
+                    "code": "jne",
+                    "name": "Jalur Nugraha Ekakurir(JNE)",
+                  },
+                  {
+                    "code": "tiki",
+                    "name": "Titipan Kilat(TIKI)",
+                  },
+                  {
+                    "code": "pos",
+                    "name": "Perusahaan Operasional Surat(POS)",
+                  }
+                ],
+                label: "Kirim Lewat",
+                hint: "Pilih kirim",
+      
+                onChanged: (value) {
+                  if (value != null) {
+                    controller.kirimLewat.value = value["code"];
+                    controller.showOngkir();
+                  } else {
+                    controller.hidOngkir.value = true;
+                    controller.kirimLewat.value = "";
+                  }
                 },
-                {
-                  "code": "tiki",
-                  "name": "Titipan Kilat(TIKI)",
-                },
-                {
-                  "code": "pos",
-                  "name": "Perusahaan Operasional Surat(POS)",
-                }
-              ],
-              label: "Kirim Lewat",
-              hint: "Pilih kirim",
-
-              onChanged: (value) {
-                if (value != null) {
-                  controller.kirimLewat.value = value["code"];
-                  controller.showOngkir();
-                } else {
-                  controller.hidOngkir.value = true;
-                  controller.kirimLewat.value = "";
-                }
-              },
-              itemAsString: (item) => "${item!['name']}",
-
-              popupItemBuilder: (context, item, isSelected) => Container(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  "${item['name']}",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
+                itemAsString: (item) => "${item!['name']}",
+      
+                popupItemBuilder: (context, item, isSelected) => Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "${item['name']}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Obx(
-            () => controller.hidOngkir.isTrue
-                ? SizedBox()
-                : ElevatedButton(
-                    onPressed: () => controller.totalOngkir(),
-                    child: Text("Cek Ongkos Kirim"),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
+            Obx(
+              () => controller.hidOngkir.isTrue
+                  ? SizedBox()
+                  : ElevatedButton(
+                      onPressed: () => controller.totalOngkir(),
+                      child: Text("Cek Ongkos Kirim"),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                        ),
+                        primary: Colors.blueGrey,
                       ),
-                      primary: Colors.blueGrey,
                     ),
-                  ),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
